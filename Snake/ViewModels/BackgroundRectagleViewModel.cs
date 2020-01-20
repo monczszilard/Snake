e31137bbab7df7274ebf69059c9cd1a254335ca8
Mendelovo Gymnázium <chwistekkaja@gmail.com>
2020-01-20 15:06:21 +0100

    ViewModels Created

---
+++ Snake/ViewModels/BackgroundRectagleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Snake.ViewModels
+{
+    public class BackgroundRectagleViewModel
+    {
+        public Point Position { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+    }
+}
---
+++ Snake/ViewModels/GameViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snake.ViewModels
+{
+    public class GameViewModel
+    {
+        Game Game { get; }
+
+        public GameViewModel(Game game)
+        {
+            this.Game = game;
+            this.BackgroundRectangleViewModels = new BackgroundRectagleViewModel[((int)(game.BoardSize.Height * game.BoardSize.Width))];
+        }
+
+        private void GenerateBackGround()
+        {
+            for(int row = 0; row < this.Game.BoardSize.Height; row++)
+            {
+                for (int column = 0; column < this.Game.BoardSize.Width; column++)
+                {
+                    this.BackgroundRectangleViewModels[column + row * (int)this.Game.BoardSize.Width] =
+                        new BackgroundRectagleViewModel() { Position = new Point(row, column) };
+                }
+            }
+        } 
+
+        BackgroundRectagleViewModel[] BackgroundRectangleViewModels { get; set; }
+
+    }
+}
